1710ecc24d4fdee6bcc72a11b7b385447172e192
Salomón Córdova <salojc2006@gmail.com>
2023-11-17 22:26:28 -0300

    agrego faltantes

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,9 @@ php artisan passport:install
 php artisan cache:clear
 php artisan config:clear
 
+npm install -g serverless
+serverless plugin install -n serverless-prune-plugin
+serverless plugin install -n serverless-lift
+
+serverless prune -n 1
 serverless deploy --stage staging --force 
